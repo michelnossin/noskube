07f1b083e9c355f466e6a5fddeb94dd324574e4d
Michel Nossin <michel.nossin@gmail.com>
2019-09-11 21:24:08 +0200

    Removing App from main

--- nosKube.py
+++ nosKube.py
@@ -1,28 +1,5 @@
-import npyscreen
-
 from kubernetes.kubernetes_api_factory import KubernetesApiFactory
-from kubernetes.kubernetes_api import KubernetesApi
-
-from ui.events.event_handler import EventHandler
-from ui.events.context_event_handler import ContextEventHandler
-from ui.events.cluster_event_handler import ClusterEventHandler
-
-from ui.forms.main_form import MainForm
-from ui.forms.items.status_box import StatusBox
-from ui.forms.items.context_selector import ContextSelector
-from ui.forms.items.cluster_selector import ClusterSelector
-
-class App(npyscreen.StandardApp):
-    def __init__(self, kubernetes_api):
-        super().__init__()
-        self.kubernetes_api = kubernetes_api
-
-    def update_kubernetes_api(self, kubernetes_api):
-        self.kubernetes_api = kubernetes_api
-
-    def onStart(self):
-        self.addForm("MAIN", MainForm, name="NosKube Professional Edition")
-
+from ui.app.app import App
 
 if __name__ == "__main__":
     kubernetes_api = KubernetesApiFactory().build_kubernetes_api()
---
+++ ui/app/app.py
@@ -0,0 +1,15 @@
+import npyscreen
+
+from ui.forms.main_form import MainForm
+
+class App(npyscreen.StandardApp):
+    def __init__(self, kubernetes_api):
+        super().__init__()
+        self.kubernetes_api = kubernetes_api
+
+    def update_kubernetes_api(self, kubernetes_api):
+        self.kubernetes_api = kubernetes_api
+
+    def onStart(self):
+        self.addForm("MAIN", MainForm, name="NosKube Professional Edition")
+
